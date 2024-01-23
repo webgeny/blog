8613c5b533e9b589ae4ec2b347c7c0ae10d9207a
evolovlikov <evgenyvolovlikov@gmail.com>
2024-01-23 17:02:44 +0300

    add className util

--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,13 @@ import {Link, Route, Routes} from 'react-router-dom';
 import { AboutPageAsync } from './pages/about-page/about-page.async';
 import { MainPageAsync } from './pages/main-page/main-page.async';
 import { useTheme } from './theme/use-theme';
+import { classNames } from './helpers/classnames/classnames';
 import './styles/index.scss';
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme()
     return (
-      <div className={`app ${theme}`}>
+      <div className={classNames(`app`, {}, [theme])}>
         <button onClick={toggleTheme}>Toggle</button>
           <Link to='/'>Главная</Link>
           <Link to='/about'>О сайте</Link>
